74aabbce9edbc472a9b7af5e493b8e51a9351e90
pyxl <pyxl@skill.issue>
2022-04-09 15:25:43 +0200

    :white_check_mark: Test rename all files in a directory

--- tests/test_main.py
+++ tests/test_main.py
@@ -26,3 +26,22 @@ class Test(TestCase):
         part_to_add = 'something'
         self.file_path = main.rename_file(self.file_path, part_to_remove, part_to_add)
         self.assertEqual(self.file_path, Path("./test_something_manipulation.txt"))
+
+    def test_rename_all_files(self):
+        print("test_rename_all_files")
+        # Create a directory
+        self.dir_path = Path('./test_dir_manipulation')
+        self.dir_path.mkdir()
+        # Create multiple files
+        for i in range(3):
+            self.dir_path.joinpath(f'file_{i}.txt').touch()
+        # Rename all files
+        main.rename_all_files(self.dir_path, 'file', 'something')
+        # Check if all files are renamed
+        for i in range(3):
+            self.assertEqual(self.dir_path.joinpath(f'something_{i}.txt'), Path(f'./test_dir_manipulation/something_{i}.txt'))
+        # Remove files
+        for i in range(3):
+            self.dir_path.joinpath(f'something_{i}.txt').unlink()
+        # Remove directory
+        self.dir_path.rmdir()
